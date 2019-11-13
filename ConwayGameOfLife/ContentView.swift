72b99f2c38aeb8955cd7516f10c82c72f04f0515
Tomás <truizlop@gmail.com>
2019-11-13 09:47:50 +0100

    Initial Commit

---
+++ ConwayGameOfLife/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConwayGameOfLife
+//
+//  Created by Tomás Ruiz López on 13/11/2019.
+//  Copyright © 2019 Bow Authors. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
