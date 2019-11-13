d38c151b3209a60ad4f123668093aef7009281a4
Tomás <truizlop@gmail.com>
2019-11-13 13:25:27 +0100

    Bind everything to the view

--- ConwayGameOfLife/ContentView.swift
+++ ConwayGameOfLife/ContentView.swift
@@ -1,16 +1,47 @@
-//
-//  ContentView.swift
-//  ConwayGameOfLife
-//
-//  Created by Tomás Ruiz López on 13/11/2019.
-//  Copyright © 2019 Bow Authors. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var store = Store(initialState: Pattern.glider.grid, reducer: conwayReducer)
+    @State var isStarted = false
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            grid.navigationBarTitle("Conway's Game of Life")
+                .navigationBarItems(trailing: self.startStopButton)
+        }
+    }
+    
+    var grid: some View {
+        VStack {
+            ForEach(store.state, id: \.self) { row in
+                HStack {
+                    ForEach(row, id: \.self) { cell in
+                        Text(cell.description)
+                            .font(.title)
+                    }
+                }
+            }
+        }
+    }
+    
+    var startStopButton: some View {
+        if isStarted {
+            return Button("Stop") {
+                self.isStarted = false
+            }
+        } else {
+            return Button("Start") {
+                self.isStarted = true
+                self.simulateGame()
+            }
+        }
+    }
+    
+    func simulateGame() {
+        if isStarted {
+            store.send(.stepSimulation)
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(300), execute: simulateGame)
+        }
     }
 }
 
