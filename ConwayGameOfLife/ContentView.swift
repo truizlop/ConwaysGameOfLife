4599133a8fc2c26cc6a893894ff9237a5cb970a4
Tomás <truizlop@gmail.com>
2019-11-13 16:28:15 +0100

    Add modal view to select the initial pattern

--- ConwayGameOfLife/ContentView.swift
+++ ConwayGameOfLife/ContentView.swift
@@ -3,11 +3,20 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var store = Store(initialState: Pattern.glider.grid, reducer: conwayReducer)
     @State var isStarted = false
+    @State var showPicker = false
     
     var body: some View {
         NavigationView {
-            grid.navigationBarTitle("Conway's Game of Life")
-                .navigationBarItems(trailing: self.startStopButton)
+            VStack {
+                Spacer()
+                grid
+                Spacer()
+                startStopButton
+            }.navigationBarTitle("Conway's Game of Life")
+        }.sheet(isPresented: $showPicker) {
+            PatternPicker { pattern in
+                self.store.send(.select(pattern))
+            }
         }
     }
     
@@ -25,22 +34,48 @@ struct ContentView: View {
     }
     
     var startStopButton: some View {
-        if isStarted {
-            return Button("Stop") {
-                self.isStarted = false
+        HStack {
+            Button("Select pattern") {
+                self.showPicker = true
+            }.disabled(isStarted)
+            
+            Spacer()
+            
+            if isStarted {
+                Button("Stop") {
+                    self.isStarted = false
+                }
+            } else {
+                Button("Start") {
+                    self.isStarted = true
+                    self.simulateGame()
+                }
             }
-        } else {
-            return Button("Start") {
-                self.isStarted = true
-                self.simulateGame()
-            }
-        }
+        }.padding()
     }
     
     func simulateGame() {
         if isStarted {
             store.send(.stepSimulation)
             DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(300), execute: simulateGame)
+        }
+    }
+}
+
+struct PatternPicker: View {
+    @Environment(\.presentationMode) var presentation
+    let select: (Pattern) -> Void
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(Pattern.allCases, id: \.self) { pattern in
+                    Button(pattern.title) {
+                        self.select(pattern)
+                        self.presentation.wrappedValue.dismiss()
+                    }
+                }
+            }.navigationBarTitle("Select pattern")
         }
     }
 }
