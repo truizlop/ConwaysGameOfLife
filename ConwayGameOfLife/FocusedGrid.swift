56130c0fbd71f50a32a051f849239c0ef5b208c7
Tomás <truizlop@gmail.com>
2019-11-13 10:23:41 +0100

    Initial implementation of FocusedGrid

---
+++ ConwayGameOfLife/FocusedGrid.swift
@@ -0,0 +1,32 @@
+import Bow
+
+public final class ForFocusedGrid {}
+public typealias FocusedGridOf<A> = Kind<ForFocusedGrid, A>
+
+public final class FocusedGrid<A>: FocusedGridOf<A> {
+    let focus: (x: Int, y: Int)
+    let grid: [[A]]
+    
+    static func fix(_ value: FocusedGridOf<A>) -> FocusedGrid<A> {
+        value as! FocusedGrid<A>
+    }
+    
+    init(focus: (x: Int, y: Int), grid: [[A]]) {
+        self.focus = focus
+        self.grid = grid
+    }
+    
+    subscript(at: (Int, Int)) -> A {
+        self[at.0, at.1]
+    }
+    
+    subscript(x: Int, y: Int) -> A {
+        let x = (x + grid.count) % grid.count
+        let y = (y + grid[x].count) % grid[x].count
+        return grid[x][y]
+    }
+}
+
+public postfix func ^<A>(_ value: FocusedGridOf<A>) -> FocusedGrid<A> {
+    FocusedGrid.fix(value)
+}
