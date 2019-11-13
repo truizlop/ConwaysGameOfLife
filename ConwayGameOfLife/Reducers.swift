9e911553d04710ec4323f6e1308d6cacd7688bb6
Tomás <truizlop@gmail.com>
2019-11-13 16:27:07 +0100

    Add action to select a pattern

--- ConwayGameOfLife/Reducers.swift
+++ ConwayGameOfLife/Reducers.swift
@@ -1,9 +1,11 @@
 public enum ConwayAction {
     case stepSimulation
+    case select(Pattern)
 }
 
 public func conwayReducer(state: [[Cell]], action: ConwayAction) -> [[Cell]] {
     switch action {
     case .stepSimulation: return gameOfLife(state)
+    case let .select(pattern): return pattern.grid
     }
 }
